000570e0d8872f017f2604fd842d1b693e13dc0d
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2015-12-26 22:56:39 +0100

    Update to Symfony 3

--- app/AppCache.php
+++ app/AppCache.php
@@ -4,11 +4,6 @@ require_once __DIR__.'/AppKernel.php';
 
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
-/**
- * Class AppCache
- *
- * @license http://opensource.org/licenses/mit-license.php MIT
- */
 class AppCache extends HttpCache
 {
 }
