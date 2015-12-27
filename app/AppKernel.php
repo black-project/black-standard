771195c401de5f65ce2ba4b4f81afe9172689c29
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2015-12-27 14:30:39 +0100

    Re-roll to Symfony 2.8 and add Puli
    I'm fuckin tired of self.version

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Puli\SymfonyBundle\PuliBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
