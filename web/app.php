0f2e6e3e2bfa63001943d4e50d7122c188793cba
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2016-06-30 16:11:29 +0200

    Add Symfony-Standard v3.1.1 updates

--- web/app.php
+++ web/app.php
@@ -1,20 +1,9 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
-
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
